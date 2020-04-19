d39c83ab0636cf66c35dbb9696f987c98e44c471
jiaochuanyang <1912597074@qq.com>
2020-04-19 19:31:50 +0800

    小球进入购物车动画实现

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -27,6 +27,7 @@ export default {
       } else {
         this.food.count++
       }
+      this.$dispatch('cart.add', event.target)
     },
     decreaseCart(event) {
       if (!event._constructed) {
